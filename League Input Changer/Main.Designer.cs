dbbd008672fc8fc51e0c765a0ccd4b60d1edf574
Neil <neil.fowlie@gmail.com>
2015-11-05 21:56:46 -0500

    Alpha release

---
+++ League Input Changer/Main.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace League_Input_Changer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filesDropDown = new System.Windows.Forms.ComboBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(73, 142);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.filesDropDown);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "League Input Changer";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ComboBox filesDropDown;
+        private System.Windows.Forms.Button loadButton;
+
+    }
+}
+
---
+++ League Input Changer/Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace League_Input_Changer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public DirectoryInfo RootPath = new DirectoryInfo("C:\\Riot Games\\League of Legends\\Config");
+        public DirectoryInfo InputPath = new DirectoryInfo("C:\\Riot Games\\League of Legends\\Config\\input");
+
+        public FileInfo[] FileRetrieval(DirectoryInfo input)
+        {
+            FileInfo[] files = input.GetFiles();
+            filesDropDown.DataSource = files;
+
+            return files;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            if(!InputPath.Exists){
+                InputPath.Create();
+            }
+            
+            var files = FileRetrieval(InputPath);
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            FileInfo file = filesDropDown.SelectedItem as FileInfo;
+            
+            File.Copy(InputPath + "\\" + file.Name, RootPath + "\\input.ini", true);
+            //System.Diagnostics.Process.Start(@InputPath + "\\" + file.Name.ToString());
+            //FileStream fileStream = new FileStream(@InputPath + "\\" + file.Name.ToString(), FileMode.Open, FileAccess.ReadWrite);
+            
+            //string[] lines = File.ReadAllLines(InputPath + "\\" + file.Name.ToString());
+        }
+    }
+}
